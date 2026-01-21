8a7dac16a83872bc639546be0f21ae06551e7ea5
adityamakkar000 <aditymakkar000@gmail.com>
2026-01-20 21:34:12 -0500

    update for train step

--- src/trainer/steps.py
+++ src/trainer/steps.py
@@ -1,9 +1,12 @@
 
+import jax.numpy as jnp
+from jaxtyping import PyTree
+from stax import modelBase
 
 
-
-def sft_step(model, params, opt_state, batch, key, train):
-    ...
+def sft_step(model : modelBase, params : PyTree,  batch : PyTree, train : bool):
+    #TODO: implement
+    return jnp.zeros((1,))
 
 def standard_rl_step():
     pass
