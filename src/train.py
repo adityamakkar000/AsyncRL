229b9844b1f352d74602551391994cd33acec02f
adityamakkar000 <aditymakkar000@gmail.com>
2026-01-21 20:47:37 -0500

    new trainer code

--- src/train.py
+++ src/train.py
@@ -11,15 +11,15 @@ cs.store(name="base", node=TrainerConfig)
 
 @hydra.main(version_base=None, config_path="./configs/train")
 def main(cfg: DictConfig) -> None:
-
     logger.info("Training Configuration:")
     logger.info("\n" + OmegaConf.to_yaml(cfg))
 
-    trainer = Trainer(config=cfg)
     try:
+        trainer = Trainer(config=cfg)
         trainer.train()
     finally:
         trainer.finish()
+
 
 if __name__ == "__main__":
     main()
