b16c2e81ea93cc0d38ec85caa642c263aecc15d1
adityamakkar000 <aditymakkar000@gmail.com>
2026-01-25 23:19:03 -0500

    pull single from checkpoint method

--- src/eval.py
+++ src/eval.py
@@ -1,4 +1,8 @@
+import os
+
+os.environ["JAX_PLATFORMS"] = "cpu"
 import hydra
+import jax
 from hydra.core.config_store import ConfigStore
 from loguru import logger
 from omegaconf import DictConfig, OmegaConf
@@ -12,9 +16,9 @@ cs.store(name="base", node=evalConfig)
 
 @hydra.main(version_base=None, config_path="./configs/eval")
 def main(cfg: DictConfig) -> None:
-    logger.info("Evaluation Configuration:")
-    logger.info("\n" + OmegaConf.to_yaml(cfg))
-
+    logger.info(f"Evaluation Configuration: \n{OmegaConf.to_yaml(cfg)}")
+    logger.info(f"Devices: {jax.devices()}")
+    
     eval_runner = EvalRunner(config=cfg)
     eval_runner.run_evaluation()
 
