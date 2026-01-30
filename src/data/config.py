c6a336ed14556b4131b1af25340810e2d4ec408f
adityamakkar000 <aditymakkar000@gmail.com>
2026-01-29 22:16:42 -0500

    basic rl losses implmented

--- src/data/config.py
+++ src/data/config.py
@@ -49,6 +49,7 @@ class DataConfig:
 @struct.dataclass
 class RLBatch:
     tokens: jax.Array  # [batch_size, max_seq_len]
-    seq_lens: jax.Array  # [batch_size, ] what is the length of each sequence 
-    token_train_lens: jax.Array  # [batch_size, ] which tokens are LLM completion
-    rewards: jax.Array  # [batch_size, ], reward at each token
+    reference_model_logprobs: jax.Array  # [batch_size, max_seq_len]
+    seq_lens: jax.Array  # [batch_size, ] what is the length of each sequence
+    rewards: jax.Array  # [batch_size, max_seq_len], reward at each token
+    token_mask: jax.Array  # [batch_size, max_seq_len], which tokens to train on
