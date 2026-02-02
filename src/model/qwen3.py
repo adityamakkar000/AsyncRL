fix numerical issues:

src/model/qwen3.py
@@ -7,67 +7,67 @@ from flax import linen as nn
 from jaxtyping import Array
 
 from .config import KVCache, QwenConfig
-from .utils import convert_dtype, make_attention_mask
-
+from .utils import convert_dtype, make_attention_mask, make_prompt_mask
 
 class FeedForward(nn.Module):
     d_ff: int
     model_dim: int
-    model_dtype: jnp.dtype = jnp.float32
+    activation_dtype: jnp.dtype = jnp.float32
 
     @nn.compact
     def __call__(self, x: Array):
-        x_fc1 = nn.Dense(features=self.d_ff, use_bias=False, dtype=self.model_dtype)(x)
-        x_fc2 = nn.Dense(features=self.d_ff, use_bias=False, dtype=self.model_dtype)(x)
+        x_fc1 = nn.Dense(features=self.d_ff, use_bias=False, dtype=self.activation_dtype)(x)
+        x_fc2 = nn.Dense(features=self.d_ff, use_bias=False, dtype=self.activation_dtype)(x)
         x = nn.silu(x_fc1) * x_fc2
-        x_fc3 = nn.Dense(features=self.model_dim, use_bias=False, dtype=self.model_dtype)(x)
+        x_fc3 = nn.Dense(features=self.model_dim, use_bias=False, dtype=self.activation_dtype)(x)
 
         return x_fc3
 
 
 class RMSNorm(nn.Module):
-    model_dtype: jnp.dtype = jnp.float32
-    shift: bool = False
+    activation_dtype: jnp.dtype = jnp.float32
 
     @nn.compact
     def __call__(self, x: Array):
-        eps = 1e-6
-        x /= jnp.sqrt(jnp.mean(jnp.square(x), axis=-1, keepdims=True) + eps)
-
-        gamma = self.param("gamma", nn.initializers.ones, (x.shape[-1]), self.model_dtype)
-        beta = self.param("beta", nn.initializers.ones, (x.shape[-1]), self.model_dtype) if self.shift else None
-
-        x = x * gamma
-
-        if self.shift:
-            x += beta
-
+        rms= jnp.sqrt(jnp.mean(jnp.square(x.astype(jnp.float32)), axis=-1, keepdims=True) + 1e-6)
+        gamma = self.param("gamma", nn.initializers.ones, (x.shape[-1]), self.activation_dtype)
+        x = (x * gamma)/rms
         return x
 
 
 class RoPE(nn.Module):
     sequence_len: int
     model_dim: int
-    model_dtype: jnp.dtype = jnp.float32
+    rope_base: int
 
     def setup(self):
         assert self.model_dim % 2 == 0, "Hidden dimension must be even"
 
-        m = jnp.arange(0, self.sequence_len, dtype=self.model_dtype)
-        pos = jnp.arange(0, self.model_dim, 2, dtype=self.model_dtype) / self.model_dim
-        theta = 1.0 / (1000000**pos)
+        m = jnp.arange(0, self.sequence_len, dtype=jnp.float32)
+        pos = jnp.arange(0, self.model_dim, 2, dtype=jnp.float32) / self.model_dim
+        theta = 1.0 / (self.rope_base **pos)
 
-        inp = jnp.einsum("t,k->tk", m, theta)
+        inp = jnp.einsum("t,k->tk", m, theta, precision=jax.lax.Precision.HIGHEST)
 
-        self.sin = jnp.sin(inp)
-        self.cos = jnp.cos(inp)
+        self.sin = jnp.sin(inp).astype(jnp.float32)
+        self.cos = jnp.cos(inp).astype(jnp.float32)
 
-    def __call__(self, x: Array, t_start: int):
+    def __call__(self, x: Array, index_map: Array):
         x = einops.rearrange(x, "b t g d -> b g t d")
         B, h, T, C = x.shape
 
-        cos = jax.lax.dynamic_slice(self.cos, (t_start, 0), (T, self.cos.shape[-1]))[None, None]
-        sin = jax.lax.dynamic_slice(self.sin, (t_start, 0), (T, self.sin.shape[-1]))[None, None]
+        index_map = index_map.reshape(-1)
+
+        @jax.vmap
+        def get_single_sin_cos_row(input):
+            sin = jax.lax.dynamic_slice(self.sin, (input, 0), (1, C // 2))
+            cos = jax.lax.dynamic_slice(self.cos, (input, 0), (1, C // 2))
+            return sin, cos
+
+        sin, cos = jax.tree.map(
+            lambda x: x.reshape(B, T, C//2)[:, None, ...],
+            get_single_sin_cos_row(index_map)
+        )
 
         x1, x2 = x[..., : C // 2], x[..., C // 2 :]
 
@@ -81,11 +81,10 @@ class GroupedQueryAttention(nn.Module):
     model_dim: int
     n_heads: int
     n_groups: int
-    q_norm: bool
-    k_norm: bool
     max_sequence_len: int
-    head_dim: int = 64
-    model_dtype: jnp.dtype = jnp.float32
+    head_dim: int 
+    rope_base: int 
+    activation_dtype: jnp.dtype = jnp.float32
 
     def setup(self):
         assert self.n_heads % self.n_groups == 0, "Number of heads must be divisible by number of kv groups"
@@ -99,30 +98,33 @@ class GroupedQueryAttention(nn.Module):
         B, T, C = x.shape
         t_start = kv_cache.length if kv_cache else 0
 
-        q = nn.Dense(features=self.d_out, use_bias=False, dtype=self.model_dtype)(x)
+        q = nn.Dense(features=self.d_out, use_bias=False, dtype=jnp.float32)(x)
         k = nn.Dense(
             features=self.n_groups * self.head_dim,
             use_bias=False,
-            dtype=self.model_dtype,
+            dtype=jnp.float32
         )(x)
         v = nn.Dense(
             features=self.n_groups * self.head_dim,
             use_bias=False,
-            dtype=self.model_dtype,
+            dtype=jnp.float32
         )(x)
 
         q = einops.rearrange(q, "... t (h d) -> ... t h d", d=self.head_dim)
         k = einops.rearrange(k, "... t (g d) -> ... t g d", d=self.head_dim)
         v = einops.rearrange(v, "... t (g d) -> ... t g d", d=self.head_dim)
 
-        if self.q_norm:
-            q = RMSNorm(self.model_dtype)(q)
+        q = RMSNorm(self.activation_dtype)(q)
+        k = RMSNorm(self.activation_dtype)(k)
 
-        if self.k_norm:
-            k = RMSNorm(self.model_dtype)(k)
 
-        queries = RoPE(self.max_sequence_len, q.shape[-1], self.model_dtype)(q, t_start)
-        k = RoPE(self.max_sequence_len, k.shape[-1], self.model_dtype)(k, t_start)
+        prompt_mask = make_prompt_mask(T, seq_lens)
+        index_map = jnp.cumsum(prompt_mask, axis=-1)
+        # account for kv_cache length
+        index_map_with_offset = jnp.where(index_map > 0, index_map + t_start - 1, 0)
+
+        q = RoPE(self.max_sequence_len, self.head_dim, self.rope_base)(q, index_map_with_offset)
+        k = RoPE(self.max_sequence_len, self.head_dim, self.rope_base) (k, index_map_with_offset)
 
         if kv_cache:
             k_cache, v_cache = kv_cache.k, kv_cache.v
@@ -134,27 +136,28 @@ class GroupedQueryAttention(nn.Module):
             )
             kv_cache = KVCache(k=k_cache, v=v_cache, length=t_start + T)
 
-        queries = einops.rearrange(queries, pattern="b t (g r) d -> b t g r d", g=k.shape[2])
+        q = einops.rearrange(q, pattern="b t (g r) d -> b t g r d", g=k.shape[-2])
 
         wei = jnp.einsum(
-            "btgrd, bTgd -> btTgr",
-            queries.astype(jnp.float32),
-            k.astype(jnp.float32),
-        ) / jnp.sqrt(self.head_dim)
+            "btgrd, bTgd -> btTgr", q, k
+        ) * (self.head_dim ** -0.5)
 
-        wei = einops.rearrange(wei, pattern="b t T g r -> b t T (g r)")
+        wei = einops.rearrange(wei, pattern="b t T g r -> b (g r) t T ")
 
         attention_mask = make_attention_mask(t=T, T=k.shape[1], seq_lens=seq_lens)
         wei = jnp.where(attention_mask == 1, wei, -jnp.inf)
-        wei = jax.nn.softmax(wei, axis=-2)
+        wei = jax.nn.softmax(wei, axis=-1)
         nan_mask = ~jnp.isnan(wei)
         wei = jnp.where(nan_mask == 1, wei, 0)
 
-        wei = einops.rearrange(tensor=wei, pattern="b t T (g r) -> b t T g r", g=k.shape[2])
+        wei = einops.rearrange(tensor=wei, pattern="b (g r ) t T -> b t T g r", g=k.shape[2])
 
-        out = jnp.einsum("btTgr, bTgd -> btgrd", wei, v.astype(jnp.float32))
+        out = jnp.einsum("btTgr, bTgd -> btgrd", wei, v)
+        out = out.astype(x.dtype)
+
         out = einops.rearrange(out, "b t g r d -> b t (g r d)")
-        out = nn.Dense(features=self.model_dim, use_bias=False, dtype=self.model_dtype)(out)
+        out = nn.Dense(features=self.model_dim, use_bias=False, dtype=self.activation_dtype)(out)
+
         return out, kv_cache
 
 
@@ -165,28 +168,28 @@ class Block(nn.Module):
     n_heads: int
     n_groups: int
     head_dim: int
-    model_dtype: jnp.dtype = jnp.float32
+    rope_base: int 
+    activation_dtype: jnp.dtype = jnp.float32
 
     @nn.compact
     def __call__(self, x: Array, seq_lens: jax.Array, layer_cache: Optional[KVCache] = None):
         connection_1 = x
-        x = RMSNorm(model_dtype=self.model_dtype)(x)
+        x = RMSNorm(activation_dtype=self.activation_dtype)(x)
         x, out_layer_cache = GroupedQueryAttention(
             model_dim=self.model_dim,
             n_heads=self.n_heads,
             n_groups=self.n_groups,
-            q_norm=True,
-            k_norm=True,
             max_sequence_len=self.sequence_len,
             head_dim=self.head_dim,
-            model_dtype=self.model_dtype,
+            rope_base=self.rope_base, 
+            activation_dtype=self.activation_dtype,
         )(x, seq_lens, layer_cache)
 
         x = x + connection_1
 
         connection_2 = x
-        x = RMSNorm(model_dtype=self.model_dtype)(x)
-        x = FeedForward(d_ff=self.d_ff, model_dim=self.model_dim, model_dtype=self.model_dtype)(x)
+        x = RMSNorm(activation_dtype=self.activation_dtype)(x)
+        x = FeedForward(d_ff=self.d_ff, model_dim=self.model_dim, activation_dtype=self.activation_dtype)(x)
         x = x + connection_2
 
         return x, out_layer_cache
@@ -201,14 +204,15 @@ class Qwen3(nn.Module):
     n_groups: int
     head_dim: int
     n_layers: int
-    model_dtype: jnp.dtype = jnp.float32
+    rope_base: int
+    activation_dtype: jnp.dtype = jnp.float32
 
     @nn.compact
     def __call__(self, x: Array, sequence_lens: jax.Array, kv_cache: Optional[list[KVCache]] = None):
         embed_layer = nn.Embed(
             num_embeddings=self.vocab_size,
             features=self.model_dim,
-            dtype=self.model_dtype,
+            dtype=self.activation_dtype,
             name="token_emb",
         )
 
@@ -224,20 +228,21 @@ class Qwen3(nn.Module):
                 n_heads=self.n_heads,
                 n_groups=self.n_groups,
                 head_dim=self.head_dim,
-                model_dtype=self.model_dtype,
+                rope_base=self.rope_base, 
+                activation_dtype=self.activation_dtype,
             )(x, sequence_lens, in_layer_cache)
 
             out_cache.append(out_layer_cache)
 
-        x = RMSNorm(model_dtype=self.model_dtype)(x)
+        x = RMSNorm(activation_dtype=self.activation_dtype)(x)
 
         # logits = embed_layer.attend(x)
-        logits = nn.Dense(features=self.vocab_size, use_bias=False, dtype=self.model_dtype)(x)
+        logits = nn.Dense(features=self.vocab_size, use_bias=False, dtype=self.activation_dtype)(x)
         return logits, out_cache if kv_cache else None
 
     @classmethod
     def from_config(cls, config: QwenConfig):
-        model_dtype = convert_dtype(config.model_dtype)
+        activation_dtype = convert_dtype(config.activation_dtype)
         return cls(
             vocab_size=config.vocab_size,
             d_ff=config.d_ff,
@@ -247,5 +252,6 @@ class Qwen3(nn.Module):
             n_groups=config.n_groups,
             head_dim=config.head_dim,
             n_layers=config.n_layers,
-            model_dtype=model_dtype,
+            rope_base=config.rope_base,
+            activation_dtype=activation_dtype,
         )

src/model/utils.py
@@ -23,6 +23,63 @@ def convert_dtype(dtype_str: str) -> jnp.dtype:
     else:
         raise ValueError(f"Unsupported dtype string: {dtype_str}")
 
+def make_prompt_mask(padding_len: int, seq_lens: jax.Array) -> jax.Array:
+    """
+    Create a prompt mask to handle left-padding in sequences.
+    Args:
+        padding_len (int): The total length of the sequences including padding.
+        seq_lens (jax.Array): Array of sequence lengths for each batch element.
+    Returns:
+        jax.Array: Prompt mask of shape (batch_size, padding_len).
+
+    example:
+        seq_lens = jnp.array([3, 5])
+        padding_len = 5
+        [[0 1 2 3 4]] >= [[2], [0]]
+        returns:
+        [[0 0 1 1 1]
+            [1 1 1 1 1]]
+    """
+
+    return jnp.arange(padding_len)[None, :] >= (padding_len - seq_lens[:, None])
+
+def make_tril_mask(t: int, T: int) -> jax.Array:
+    """
+    Create a lower triangular mask for causal attention.
+    Args:
+        t (int): Current time step or query length.
+        T (int): Total sequence length or key length.
+    Returns:
+        jax.Array: Lower triangular mask of shape (t, T) [last row will always be True].
+
+    example:
+        t = 3
+        T = 5
+
+        T_arange = [[0 1 2 3 4]]
+        query_position = [[0 1 2 3 4],
+                            [0 1 2 3 4],
+                            [0 1 2 3 4]]
+        key_position  = [[ 0 0 0 0 0],
+                            [ 1 1 1 1 1],
+                            [ 2 2 2 2 2],
+                            [ 3 3 3 3 3],
+                            [ 4 4 4 4 4]]
+        key_position[-t:] = [[2 2 2 2 2],
+                                [3 3 3 3 3],
+                                [4 4 4 4 4]]
+        returns:
+        [[True True True False False],
+            [True True True True False],
+            [True True True True True]]
+
+    """
+
+    T_arange = jnp.arange(T)[None, :]
+    query_position = jnp.repeat(T_arange, t, axis=0)
+    key_position = jnp.transpose(jnp.repeat(T_arange, T, axis=0))
+    return query_position <= key_position[-t:, :]
+
 
 def make_attention_mask(t: int, T: int, seq_lens: jax.Array) -> jax.Array:
     """
@@ -53,66 +110,9 @@ def make_attention_mask(t: int, T: int, seq_lens: jax.Array) -> jax.Array:
           [1 1 1 1 1]]]
     """
 
-    def create_prompt_mask(padding_len: int, seq_lens: jax.Array) -> jax.Array:
-        """
-        Create a prompt mask to handle left-padding in sequences.
-        Args:
-            padding_len (int): The total length of the sequences including padding.
-            seq_lens (jax.Array): Array of sequence lengths for each batch element.
-        Returns:
-            jax.Array: Prompt mask of shape (batch_size, padding_len).
-
-        example:
-            seq_lens = jnp.array([3, 5])
-            padding_len = 5
-            [[0 1 2 3 4]] >= [[2], [0]]
-            returns:
-            [[0 0 1 1 1]
-             [1 1 1 1 1]]
-        """
-
-        return jnp.arange(padding_len)[None, :] >= (padding_len - seq_lens[:, None])
-
-    def make_tril_mask(t: int, T: int) -> jax.Array:
-        """
-        Create a lower triangular mask for causal attention.
-        Args:
-            t (int): Current time step or query length.
-            T (int): Total sequence length or key length.
-        Returns:
-            jax.Array: Lower triangular mask of shape (t, T) [last row will always be True].
-
-        example:
-            t = 3
-            T = 5
-
-            T_arange = [[0 1 2 3 4]]
-            query_position = [[0 1 2 3 4],
-                              [0 1 2 3 4],
-                              [0 1 2 3 4]]
-            key_position  = [[ 0 0 0 0 0],
-                             [ 1 1 1 1 1],
-                             [ 2 2 2 2 2],
-                             [ 3 3 3 3 3],
-                             [ 4 4 4 4 4]]
-            key_position[-t:] = [[2 2 2 2 2],
-                                 [3 3 3 3 3],
-                                 [4 4 4 4 4]]
-            returns:
-            [[True True True False False],
-             [True True True True False],
-             [True True True True True]]
-
-        """
-
-        T_arange = jnp.arange(T)[None, :]
-        query_position = jnp.repeat(T_arange, t, axis=0)
-        key_position = jnp.transpose(jnp.repeat(T_arange, T, axis=0))
-        return query_position <= key_position[-t:, :]
-
-    prompt_mask = create_prompt_mask(padding_len=T, seq_lens=seq_lens)
-    tril = make_tril_mask(t, T)[None, :, :, None]  # 1,1, t, T
-    return prompt_mask[:, None, :, None] * tril
+    prompt_mask = make_prompt_mask(padding_len=T, seq_lens=seq_lens)
+    tril = make_tril_mask(t, T)[None, None, :, :]  # 1,1, t, T
+    return prompt_mask[:, None, None, :] * tril
 
 
 HF_MAPPING = {  # embedding