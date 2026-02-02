fix numerical issues:

src/model_test.py
@@ -5,8 +5,7 @@ from hydra.core.config_store import ConfigStore
 from loguru import logger
 from omegaconf import DictConfig, OmegaConf
 
-from src.model.config import ModelConfig
-from src.model.main import Model
+from src.model import Model, ModelConfig, Qwen3Model
 
 jax.config.update("jax_default_matmul_precision", "highest")
 jax.numpy.set_printoptions(precision=9)
@@ -25,13 +24,14 @@ def main(cfg: DictConfig) -> None:
     out_state = model.init_state(rng, None, sharding=None, abstract=False)
     params = out_state["params"]
 
-    x = jnp.ones((4, 5), dtype=jnp.int32)
-    seq_lens = jnp.array([1, 2, 3, 4])
+    x = jax.random.randint(jax.random.key(32), (4, 5), minval=0, maxval=10000, dtype=jnp.int32)
+    seq_lens = jnp.array([2,3,4,5])
     kv_cache = model.init_kv_cache(x)
 
-    logits, cache = model.apply({"params": params}, x=x, sequence_lens=seq_lens, kv_cache=kv_cache)
-    breakpoint()
 
+    logits, cache = model.apply({"params": params}, x=x, sequence_lens=seq_lens)
+
+    breakpoint()
 
 if __name__ == "__main__":
     main()