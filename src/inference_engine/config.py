a9b83ce2f2b60ec71112257ee2ec74714a436bbb
adityamakkar000 <adityamakkar000@gmail.com>
2026-02-03 20:34:43 +0000

    updat

--- src/inference_engine/config.py
+++ src/inference_engine/config.py
@@ -6,7 +6,9 @@ from omegaconf import MISSING
 @dataclass
 class InferenceConfig:
     temperature: float = MISSING
+    top_k: int = MISSING
     top_p: float = MISSING
     max_seq_len: int = MISSING
     batch_size: int = MISSING
     group_size: int = MISSING
+    kv_cache_dtype: str = MISSING
