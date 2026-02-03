112d812fe2c336defacecd6e159bdeedb9c1defa
adityamakkar000 <adityamakkar000@gmail.com>
2026-02-03 20:29:08 +0000

    update

--- src/model/main.py
+++ src/model/main.py
@@ -116,7 +116,7 @@ class Model(HFModelBase):
             ),
         )
         assert hasattr(restored, "state"), "Restored object has no attribute 'state'"
-        assert restored.state, "Restored state has no 'params' key"
+        assert restored.state, "Restored has no state"
 
         return step_number, restored.state["params"]
 
