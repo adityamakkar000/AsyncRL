abba0aeb412e091dffc34192f39d764a796729ce
divyamakkar0 <divyamakkar000@gmail.com>
2026-02-16 05:08:39 +0000

    integrated the inference engine

--- src/trainer/config.py
+++ src/trainer/config.py
@@ -66,7 +66,7 @@ class RLConfig:
 class LossConfig:
     rl_config: RLConfig = field(default_factory=RLConfig)
     grad_steps: int = 1  # how many off-policy ppo steps to take
-    inference: InferenceConfig = field(default_factory=InferenceConfig)
+    inference_config: InferenceConfig = field(default_factory=InferenceConfig)
     annealing_config: Optional[AnnealedLoss] = None  # TODO: Annealed RL config will go here
 
 
