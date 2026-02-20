ab2ed2c39a2e08d1f25d90bb60862e4eab519676
Chinmay <chinmayjindal108@gmail.com>
2026-02-19 21:35:07 -0500

    format

--- src/data/verifier.py
+++ src/data/verifier.py
@@ -1,16 +1,17 @@
 from dataclasses import dataclass
 import re
 
+
 @dataclass
 class VerifierInput:
     solution: str
     answer: str
 
-class Verifier:
 
+class Verifier:
     def __init__(self):
         self.box_start = re.compile(r"\\boxed\{")
-        
+
     def extract_boxed_content(self, solution: str) -> str:
         match = self.box_start.search(solution)
         if not match:
@@ -22,9 +23,9 @@ class Verifier:
         while i < len(solution):
             ch = solution[i]
 
-            if ch == '{':
+            if ch == "{":
                 cur_open += 1
-            elif ch == '}':
+            elif ch == "}":
                 cur_open -= 1
             if cur_open == 0:
                 return solution[start_idx:i]
@@ -34,7 +35,7 @@ class Verifier:
     def _get_reward(self, solution: str, answer: str) -> float:
         parsed_answer = self.extract_boxed_content(solution)
         if parsed_answer is None:
-            return 0.0 # could this be -1.0 @adityamakkar000
+            return 0.0  # could this be -1.0 @adityamakkar000
         return 1.0 if (answer in parsed_answer) else 0.0
 
     def __call__(self, input: VerifierInput) -> float:
