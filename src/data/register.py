151b3960cd6a1d004732e099334a5e5146616ffc
adityamakkar000 <aditymakkar000@gmail.com>
2026-02-24 20:36:46 -0500

    add aime and gsm8k

--- src/data/register.py
+++ src/data/register.py
@@ -37,10 +37,39 @@ def register_dataset(name: str) -> Callable[[Callable[[], list[Sample]]], Callab
 
 @register_dataset("omnimath")
 def load_omnimath() -> list[Sample]:
-
     ds = load_dataset("KbsdJames/Omni-MATH", split="test")
     samples = [
         Sample(prompt=example["problem"], answer=example["answer"], solution=example["solution"]) for example in ds
     ]
     ds.cleanup_cache_files()
     return samples
+
+
+@register_dataset("gsm8k")
+def load_gsm8k() -> list[Sample]:
+    ds = load_dataset("openai/gsm8k", "main", split="test")
+
+    def get_answer(answer: str) -> str:
+        """
+        Answers are of the form '... #### 15'
+        So we seperate, take the last part after the # and strip whitespace to get the answer.
+        """
+        return answer.split("#")[-1].strip()
+
+    samples = [
+        Sample(prompt=example["question"], answer=get_answer(example["answer"]), solution=example["answer"])
+        for example in ds
+    ]
+
+    return samples
+
+
+@register_dataset("aime")
+def load_aime() -> list[Sample]:
+    ds_2024 = load_dataset("Maxwell-Jia/AIME_2024")["train"]
+    aime_2024 = [
+        Sample(prompt=example["Problem"], answer=example["Answer"], solution=example["Solution"]) for example in ds_2024
+    ]
+    ds_2025 = load_dataset("MathArena/aime_2025")["train"]
+    aime_2025 = [Sample(prompt=example["problem"], answer=example["answer"], solution=None) for example in ds_2025]
+    return aime_2024 + aime_2025
