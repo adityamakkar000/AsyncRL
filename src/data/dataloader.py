efd49c46555d4d488b8e90d04f980f1965ccfebb
Chinmay <chinmayjindal108@gmail.com>
2026-02-24 20:51:42 -0500

    cat

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -51,8 +51,7 @@ class DataLoader:
         self.total_per_device = num_prompts // jax.process_count()
         self.start_idx = self._current_idx + self.rank * self.total_per_device
         self.process_end_idx = self.start_idx + self.total_per_device
-        process_indices = [i % self.total_samples for i in range(self.start_idx, self.process_end_idx)]
-        samples = [self.samples[i] for i in process_indices]
+        samples = [self.samples[i % self.total_samples] for i in range(self.start_idx, self.process_end_idx)]
         self._last_samples = samples
         self._current_idx = (self._current_idx + num_prompts) % self.total_samples
 
